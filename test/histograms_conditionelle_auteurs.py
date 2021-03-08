c8571cd8468c54d1292fd9d92892ba7f3e9e9868
Luc <luc.sterker@laposte.net>
2021-03-08 17:06:16 +0100

    Histogram update

--- test/histograms_conditionelle_auteurs.py
+++ test/histograms_conditionelle_auteurs.py
@@ -29,36 +29,65 @@ dico = make_author_database(statements)
 
 num = ['true','barely-true', 'false', 'half-true', 'mostly-true', 'pants-fire']
 
-
 new_dico = {}
+new_dico['true']          = list(dico['score'][:,[0]].sum(axis = 1))
+new_dico['mostly-true']   = list(dico['score'][:,[0,4]].sum(axis = 1))
+new_dico['half-true']     = list(dico['score'][:,[0,4, 3]].sum(axis = 1))
+new_dico['barely-true']   = list(dico['score'][:,[0,4,3,1]].sum(axis = 1))
+new_dico['false']         = list(dico['score'][:,[0,4,3,1,2]].sum(axis = 1))
+new_dico['pants-fire']    = list(dico['score'].sum(axis = 1))
+new_dico['job']           = dico["job"]
+new_dico['state']         = dico["state"]
+new_dico['nb_post']       = list(dico['score'].sum(axis = 1))
+new_dico['party']         = dico["party"]
 
-new_dico['true_score']   = list(dico['score'][:,[0,4]].sum(axis = 1))
-new_dico['neutre_score'] = list(dico['score'][:,[0,1,4,3]].sum(axis = 1))
-new_dico['false_score']  = list(dico['score'][:,[2,5]].sum(axis = 1))
+def print_histo(y):
 
-new_dico['nb_post'] = list(dico['score'].sum(axis = 1))
+    
+    #sns.set_color_codes("pastel")
+    sns.barplot(y, x="pants-fire",  data=new_dico,ci = None, orient = 'h',\
+                label="Pants on fire", color = (1, 0, 0))
+    sns.barplot(y, x="false",  data=new_dico,ci = None, orient = 'h',\
+                label="False",         color=(1, 0.25, 0.25))
+    sns.barplot(y, x="barely-true", data=new_dico,ci = None, orient = 'h',\
+                label="Barely true", color=(1, 0.5, 0.5))
+    sns.barplot(y, x="half-true",  data=new_dico,ci = None, orient = 'h',\
+                label="Half true", color=(0.5, 1, 0.5))
+    sns.barplot(y, x="mostly-true", data=new_dico,ci = None, orient = 'h',\
+                label="Mostly true", color=(0.4, 0.8, 0.4))
+    sns.barplot(y, x="true",  data=new_dico,ci = None, orient = 'h',\
+                label="True", color=(0.1, 0.8, 0.1))
+# Plot the crashes where alcohol was involved
 
-new_dico['party'] = dico["party"]
+"""
+Afficher les états
+"""
+plt.rcdefaults()
+sns.set_theme()
+fig, ax = plt.subplots(1,1, figsize = (15,20))
+print_histo("party")
+ax.set_title("Distribution des auteurs par partie")
+ax.set_ylabel("Parties")
+ax.set_xlabel("Nombre de message")
+ax.legend()
+plt.show()
 
 
 plt.rcdefaults()
 sns.set_theme()
-fig, ax = plt.subplots(1,1, figsize = (15,10))
+fig, ax = plt.subplots(1,1, figsize = (10,1))
 
-sns.set_color_codes("pastel")
-sns.barplot(y="party", x="nb_post", data=new_dico,ci = None,orient = 'h', label="false_score", color="r")
-sns.barplot(y="party", x="neutre_score", data=new_dico,ci = None, orient = 'h', label="true_score", color="b")
-sns.barplot(y="party", x="true_score", data=new_dico,ci = None, orient = 'h', label="neutre_score", color="g")
-
-# Plot the crashes where alcohol was involved
-
-"""
-ax.set_yticks(y_pos)
-ax.set_yticklabels(name_party[1:], rotation = 0)
-ax.set_title("Distribution des auteurs par partie")
-"""
-ax.set_ylabel("Parties")
-ax.set_xlabel("Nombre de message")
-
-ax.legend()
+#sns.set_color_codes("pastel")
+sns.barplot(x="pants-fire",  data=new_dico,ci = None, orient = 'h',\
+            label="Pants on fire", color = (1, 0, 0))
+sns.barplot(x="false",  data=new_dico,ci = None, orient = 'h',\
+            label="False",         color=(1, 0.25, 0.25))    
+sns.barplot(x="barely-true", data=new_dico,ci = None, orient = 'h',\
+            label="Barely true", color=(1, 0.5, 0.5))    
+sns.barplot(x="half-true",  data=new_dico,ci = None, orient = 'h',\
+            label="Half true", color=(0.5, 1, 0.5))    
+sns.barplot(x="mostly-true", data=new_dico,ci = None, orient = 'h',\
+            label="Mostly true", color=(0.4, 0.8, 0.4))
+sns.barplot(x="true",  data=new_dico,ci = None, orient = 'h',\
+            label="True", color=(0.1, 0.8, 0.1))
 plt.show()
